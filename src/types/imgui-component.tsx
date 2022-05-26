afce6d595357609a8f58bfad84b1ec8206005514
Jess <toxocious@gmail.com>
2022-05-25 21:01:45 -0700

    Implement array of valid Dear ImGui components

---
+++ src/types/imgui-component.tsx
@@ -0,0 +1,17 @@
+import { ImVec2, ImVec4 } from './imgui-imvec';
+
+export type ImGuiComponent = {
+  name: string;
+  label: string | undefined;
+  parent: ImGuiComponent | undefined;
+  children: ImGuiComponent[] | undefined;
+  value?: string | number | boolean | undefined;
+  properties: {
+    height: number;
+    width: number;
+    padding: ImVec2;
+    backgroundColor: ImVec4;
+    textColor: ImVec4;
+    active: boolean;
+  };
+};
---
+++ src/types/imgui-imvec.tsx
@@ -0,0 +1,17 @@
+export type ImVec2 = {
+  x: boolean | number | string;
+  y: boolean | number | string;
+};
+
+export type ImVec3 = {
+  x: boolean | number | string;
+  y: boolean | number | string;
+  z: boolean | number | string;
+};
+
+export type ImVec4 = {
+  r: boolean | number | string;
+  g: boolean | number | string;
+  b: boolean | number | string;
+  a: boolean | number | string;
+};
