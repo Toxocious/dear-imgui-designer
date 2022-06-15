d504b02878b2249c1d32c9979c4ca59b17c5d78e
Jess <toxocious@gmail.com>
2022-06-14 18:16:52 -0700

    Implement ComponentNode component

---
+++ src/components/component-node/component-node.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+import { ImGuiComponent } from '../../types/imgui-component';
+
+import './component-node.scss';
+
+export const ComponentNode = (props: any) => {
+  let childNodesTest = [];
+  if (props.children.length > 0) {
+    childNodesTest = props.children.map((childNode: ImGuiComponent) => {
+      return (
+        <ComponentNode
+          key={childNode.id}
+          name={childNode.name}
+          children={childNode.children}
+        />
+      );
+    });
+  }
+
+  return (
+    <li key={props.id}>
+      <span>{props.name}</span>
+      {childNodesTest ? childNodesTest : <></>}
+    </li>
+  );
+};
